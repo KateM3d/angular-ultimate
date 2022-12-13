641905ec51be3e8c697ba66493571cf45042c24f
KateM3d <ekaterina8medvedeva@gmail.com>
2022-12-13 17:34:50 -0500

    add form validation

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,20 +1,156 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-donut-form',
   template: `
-    <form class="donut-form" #form="ngForm">
+    <form class="donut-form" (ngSubmit)="handleSubmit(form)" #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" ngModel />
+        <input
+          type="text"
+          name="name"
+          class="input"
+          required
+          minlength="5"
+          ngModel
+          #name="ngModel"
+          [ngModelOptions]="{ updateOn: 'blur' }"
+        />
+        <ng-container *ngIf="name.invalid && name.touched">
+          <div class="donut-form-error" *ngIf="name.errors?.minlength">
+            Minimum length of the name is 5
+          </div>
+          <div class="donut-form-error" *ngIf="name.errors?.required">
+            Name is required
+          </div>
+        </ng-container>
+      </label>
+      <label>
+        <span>Icon</span>
+        <select
+          name="icon"
+          class="input input--select"
+          #icon="ngModel"
+          required
+          ngModel
+        >
+          <option *ngFor="let icon of icons" [ngValue]="icon">
+            {{ icon }}
+          </option>
+        </select>
+        <ng-container *ngIf="icon.invalid && icon.touched">
+          <div class="donut-form-error" *ngIf="icon.errors?.required">
+            Selection is required
+          </div>
+        </ng-container>
       </label>
       <label>
         <span>Price</span>
-        <input type="number" name="price" class="input" ngModel />
+        <input
+          type="number"
+          name="price"
+          class="input"
+          required
+          ngModel
+          #price="ngModel"
+        />
+        <ng-container *ngIf="price.invalid && price.touched">
+          <div class="donut-form-error" *ngIf="price.errors?.required">
+            Price is required
+          </div>
+        </ng-container>
       </label>
+
+      <div class="donut-form-radios">
+        <p class="donut-form-radios-label">Promo:</p>
+        <label>
+          <input type="radio" name="promo" [value]="undefined" ngModel />
+          <span>None</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="new" ngModel />
+          <span>New</span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="limited" ngModel />
+          <span>Limited</span>
+        </label>
+      </div>
+      <label>
+        <span>Description:</span>
+        <textarea
+          name="description"
+          class="input input--textarea"
+          required
+          ngModel
+          #description="ngModel"
+        ></textarea>
+        <ng-container *ngIf="price.invalid && price.touched">
+          <div class="donut-form-error" *ngIf="price.errors?.required">
+            Description is required
+          </div>
+        </ng-container>
+      </label>
+      <button type="submit" class="btn btn--green">Create</button>
+      <button type="button" class="btn btn--grey" (click)="form.resetForm()">
+        Reset form
+      </button>
+
+      <div class="donut-form-working" *ngIf="form.valid && form.submitted">
+        Working....
+      </div>
+
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
-  styles: [],
+  styles: [
+    `
+      .donut-form {
+        &-radios {
+          display: flex;
+          align-content: center;
+          &-label {
+            margin-right: 10px;
+          }
+          label {
+            display: flex;
+            align-items: center;
+            span {
+              color: #444;
+              margin-bottom: 0;
+            }
+          }
+        }
+        &-error {
+          color: #e66262;
+        }
+        &-working {
+          font-size: 12px;
+          font-style: italic;
+          margin: 10px 0;
+        }
+      }
+    `,
+  ],
 })
-export class DonutFormComponent {}
+export class DonutFormComponent {
+  icons: string[] = [
+    'caramel-swirl',
+    'glazed-fudge',
+    'just-chocolate',
+    'sour-supreme',
+    'strawberry-glaze',
+    'vanilla-sundae',
+    'zesty-lemon',
+  ];
+
+  handleSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      form.form.markAllAsTouched();
+      console.log('invalid');
+    }
+  }
+}
